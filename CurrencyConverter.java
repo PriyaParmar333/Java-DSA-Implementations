e2a061cab249ab338438767ce0a8f170c8295676
Priya Parmar <priyaparmar5526@gmail.com>
2026-03-05 23:01:25 +0530

    Add files via upload

---
+++ CurrencyConverter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter amount in Rupees: ");
+        double inr = sc.nextDouble();
+
+        double exchangeRate = 83; // 1 USD = 83 INR (assumed)
+
+        double usd = inr / exchangeRate;
+
+        System.out.println("Amount in USD: " + usd);
+    }
+}
