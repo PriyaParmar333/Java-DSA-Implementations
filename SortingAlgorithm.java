9812c14e767183758a280b207582bb40e59e51d0
Priya Parmar <priyaparmar5526@gmail.com>
2026-04-06 21:25:18 +0530

    Update SortingAlgorithm.java

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -3,8 +3,8 @@ public class SortingAlgorithm
 {    //Bubble Sort 
     public static void BubbleSort(int arr[])
     {
-        for(int turn=0;turn<arr.length;turn++)
-        {
+        for(int turn=0;turn<arr.length;turn++) 
+        { 
             for(int j=0;j<arr.length-1-turn;j++)
             {
                 if(arr[j]>arr[j+1]) 
